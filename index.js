9e366808fb9cceb44ed8970941f2ce949fc6000c
Herless Oliver <57732804+AdrianHates@users.noreply.github.com>
2023-08-03 03:13:35 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
+class MenuItem {
+  constructor(name, description, price, category) {
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.category = category;
+  }
+}
+
+const menu = [
+  new MenuItem('Scallop shell pies', 'Our passion for excellent food', '12.00', 'starter'),
+  new MenuItem('Brown shrimp Ranhofer', 'Our passion for the excellent food', '2.00', 'starter'),
+  new MenuItem('Moules marinière', 'Our passion for the excellent food', '6.00', 'starter'),
+  new MenuItem('Smoky salmon', 'Our passion for the excellent food','2.00', 'starter')
+  
+]
+
+const menuSelector = document.querySelectorAll('.menu-select > div > div')
+
+function mostrarMenu (contenido, event) {
+  const filtro = menu.filter( elemento => elemento.category===contenido)
+  const menuProductos = document.querySelector('.menu-productos')
+  const grupoFiltros = filtro.map( x => `<div><div><a href='#'>${x.name}</a><p>${x.description}</p></div><p>$${x.price}</p></div>`)
+  const prueba = grupoFiltros.join('')
+  menuProductos.innerHTML = prueba
+  if(event) {
+    menuSelector.forEach( x => x.style.color = 'white')
+    event.target.style.color = 'var(--c-dorado)'
+  } 
+}
+
+menuSelector.forEach( select => {  
+  const contenido = select.innerHTML.toLowerCase()
+  select.onclick = (event) => {
+    mostrarMenu(contenido, event)
+  }
+  if(select.innerHTML === 'Starter') {
+    select.style.color = 'var(--c-dorado)'
+  }
+})
+mostrarMenu('starter')
+
 const arrayFakeCarrusel = ["https://cutesolution.com/html/sushi/assets/img/slider-1.jpg", "https://cutesolution.com/html/sushi/assets/img/slider-2.jpg",
 "https://cutesolution.com/html/sushi/assets/img/slider-3.jpg"]
 
@@ -29,6 +71,28 @@ setInterval(() => {
 
 ScrollReveal().reveal('.video, .propiedades', { distance: '20px', origin: 'bottom', duration: 500 });
 
+function mostrarModal(event) {
+  const target = event.currentTarget.getAttribute('data-target');
+  
+  const modal = document.getElementById(target);
+  modal.style.display = 'flex';
+}
+
+function cerrarModal(event) {
+  const target = event.target
+  if(target.id !== 'videoFrame') {
+    const change = document.querySelector('#videoFrame')
+    change.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+    change.src = change.src
+    event.currentTarget.style.display = 'none'
+  }
+}
+
+const video = document.querySelector('#video > div > div')
+const modalVideo = document.querySelector('#videoModal')
+video.onclick = mostrarModal
+modalVideo.onclick = cerrarModal
+
 const svgS = document.querySelectorAll('.svg')
 svgS.forEach(element => {
   element.innerHTML ='<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" version="1.1" width="25px" height="25px" x="0" y="0" viewBox="0 0 24 24" style="enable-background:new 0 0 512 512" xml:space="preserve"><g><path fill="var(--c-dorado)" d="M5.508 12.591a.553.553 0 0 1-.003-.017l-1.22-7.32A1.5 1.5 0 0 0 2.805 4H2.5a.5.5 0 0 1 0-1h.306a2.5 2.5 0 0 1 2.45 2H21.5a.5.5 0 0 1 .48.637l-2 7a.5.5 0 0 1-.48.363H6.59l.125.747A1.5 1.5 0 0 0 8.195 15H19.5a.5.5 0 0 1 0 1H8.194a2.5 2.5 0 0 1-2.466-2.089zM5.424 6l1 6h12.699l1.714-6zM8 21a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0-1a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm9 1a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0-1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" data-original="#000000" opacity="1"/></g></svg>'
@@ -44,7 +108,7 @@ teamInteraction.forEach(member => {
 
 function initSlick() {
   var windowWidth = $(window).width();
-  var slidesToShow = windowWidth <= 800 ? 2 : 3;
+  var slidesToShow = windowWidth <= 600 ? 1 : windowWidth <= 870 ? 2 : 3;
 
   $('.exclusive-columnas').slick({
     slidesToShow: slidesToShow,
